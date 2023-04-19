c455687a6ab67788f285e6456a05ebc75283242d
Samuel Martins <smbra33@gmail.com>
2023-04-19 00:05:22 -0700

    create chatBox

--- src/components/chatBox/chatBox.jsx
+++ src/components/chatBox/chatBox.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import "./chatBox.scss";
+import $ from "jquery";
 
 class ChatBox extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    componentDidUpdate() {
+        $("#fade-in").toggleClass("show");
+    }
+
     render() {
 
         return (
             <div className="chatBox" id="fade-in">
+                <div className="chatHeader">
+                    <p className="chatTitle">CSM Support</p>
+                </div>
+                <div className="chatArea">
+
+                </div>
             </div>
         )
     }
