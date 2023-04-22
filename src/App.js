ad4cbec4ec72ace0cc83a40df347b7d35518ebfa
Samuel Martins <smbra33@gmail.com>
2023-04-22 01:28:40 -0700

    rewrite initial text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Look</code> {"that way -->"}
         </p>
         <a
           className="App-link"
